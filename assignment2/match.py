8f30cfd8949d6a9ffac277a8b9981d12eb564629
Antonino Libarnes <antoninolibarnes@proton.me>
2024-10-18 19:43:04 -0400

    feat: implement matching algorithm based on g-s

--- assignment2/match.py
+++ assignment2/match.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List, Tuple
+import random
 
 def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List[Tuple]:
     """
@@ -35,8 +36,80 @@ def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List
             elif (gender_pref[i] == "Women" and
                   gender_id[j] in ("Male", "Nonbinary")):
                 scores[i][j], scores[j][i] = 0, 0
+            # no implementation needed for i with pref "Bisexual"
 
-    matches = [()]
+    """
+    Require: Initialize each person to be free (unmatched)
+        while Some man is free and hasn’t proposed to every woman do
+            Choose such a man m
+            w ← 1st woman on m’s list to whom m has not yet proposed
+            if w is free then
+                Match m and w
+            else if w prefers m to her current match m′ then
+                Match m and w, and free up m′
+            else
+                w rejects m
+            end if
+        end while
+    """
+
+    # generate random list of 5 indices to be proposers
+    proposers = random.sample(range(len(gender_pref)), len(gender_pref) // 2)
+    receivers = list(set(range(len(gender_pref))) - set(proposers))
+
+    """
+    init lists of single proposers, number of proposals completed by each,
+    and a list of all the matches
+    """
+    free_proposers = list(proposers)
+    proposals = {p: 0 for p in proposers}
+    matches = {r: None for r in receivers}
+
+    """
+    this loop is verbose to show how this successfully implements
+    the necessary variation of gale-shapely
+    """
+    while free_proposers:
+        # take loner from beginning of the list
+        loner = free_proposers.pop(0)
+
+        # check if loner has proposed to all receivers
+        if proposals[loner] >= len(receivers):
+            print(f"{loner} has exhausted all proposals :\(")
+            continue
+
+        # the next receiver is the index of how many proposals the loner has done
+        receiver = receivers[proposals[loner]]
+        proposals[loner] += 1 # increase loner's proposal count
+
+        # if the score is 0, the gender preferences are incompatible
+        if scores[loner][receiver] == 0:
+            free_proposers.append(loner)
+            print(f"{loner} and {receiver} are incompatible.")
+            continue
+
+        # if the receiver is single, automatic match
+        elif matches[receiver] is None:
+            print(f"{receiver} is single so {loner} is matched")
+            matches[receiver] = loner
+
+        # if the receiver is taken, compare the scores
+        else:
+            old_match = matches[receiver]
+            print(f"{receiver} likes {old_match} {scores[receiver][old_match]}")
+            print(f"{receiver} likes {loner} {scores[receiver][loner]}")
+            # compare the receiver's preference toward each
+            if scores[receiver][loner] > scores[receiver][old_match]:
+                matches[receiver] = loner
+                free_proposers.append(old_match)
+                print(f"{loner} wins {receiver}, {old_match} is single")
+            else:
+                free_proposers.append(loner)
+                print(f"{old_match} wins {receiver}, {loner} is rejected")
+
+
+    # unfortunately some will be unpaired due to gender preferences :(
+    matches = [(proposer, receiver) for receiver, proposer in matches.items()]
     return matches
 
 if __name__ == "__main__":
